2c71a00f80ddd8720c78eb6329f94342b75366a4
MarcoProstamo <Marcopros1201@gmail.com>
2024-11-06 15:00:51 +0100

    Refactoring

--- js/main.js
+++ js/main.js
@@ -1,19 +1,41 @@
 // # Selecting DOM Elements
+const generateSingleEmail = document.querySelector("#generateSingleEmail");
+const generateListEmail = document.querySelector("#generateListEmail");
 const emailListEl = document.querySelector("#emailList");
 
 // # Var Init
 const emailNumber = 10;
-let emailHtml = ``;
 
-for (let i = 0; i < emailNumber; i++) {
+// # Fn
+function requestSingleEmail() {
   fetch("https://flynn.boolean.careers/exercises/api/random/mail")
     .then((response) => response.json())
     .then((data) => {
       emailListEl.innerHTML += `
-      <li class="list-group-item text-bg-secondary border-dark">${data.response}</li>
-      `;
+                <li class="list-group-item text-bg-secondary border-dark">${data.response}</li>
+                `;
     })
     .catch((error) => {
       console.error(error);
     });
 }
+
+function requestListEmail() {
+  for (let i = 0; i < emailNumber; i++) {
+    requestSingleEmail();
+  }
+}
+
+// # On Load
+addEventListener("load", requestListEmail);
+
+// # Email Generation Via Button
+generateSingleEmail.addEventListener("click", () => {
+  emailListEl.innerHTML = "";
+  requestSingleEmail();
+});
+
+generateListEmail.addEventListener("click", () => {
+  emailListEl.innerHTML = "";
+  requestListEmail();
+});
